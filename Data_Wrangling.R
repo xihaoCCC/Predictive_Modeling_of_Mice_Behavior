0d8912217745c39603571374a2153a2f33b3c46d
xihaoCCC <89085385+xihaoCCC@users.noreply.github.com>
2023-09-29 20:19:38 -0500

    Update Data_Wrangling.R

--- Data_Wrangling.R
+++ Data_Wrangling.R
@@ -1,7 +1,4 @@
-## Author: Handing Zhang
-## Purpose of the script: 1. Read in the original data.
-##                        2. Basic data wrangling.
-
+## Purpose of the script: wrangle the mice neural data.
 
 ## Sourcing this script, one should have all data as data frames in the environment.
 ## Naming Protocol:            "First Letter of the folder"_"Last three digits of the number of the mouse"
@@ -67,62 +64,6 @@ for (i in 1:length(mice_name_D)){
   dimnames(temp)[[2]][1:2] <- c("Y1", "Y2")
   temp %<>% 
     rename_at(vars(starts_with("V")), funs(str_replace(., "V", replacement = "X")))
-  
-  assign(name, temp)
-  
-  # remove temp from environment
-  rm("temp")
-}
-
-
-
-
-## Opposite Sex ####
-
-## Find all mice names in Opposite Sex folder.
-mice_name_O <- list.files("data/Dir_Interact/", pattern = "-*")
-
-
-## A for loop reading in all the data for Opposite Sext experiment for each mouse in the list.
-## Combine the bh and zs for each to be the final dataset for each mouse in the experiment.
-for (i in 1:2){
-  
-  filename_bh <- paste0("data/Opp_Sex/", mice_name_Z[i],  "/Day_1/Trial_002_0/binned_behavior.mat")
-  filename_zs <- paste0("data/Opp_Sex/", mice_name_Z[i],  "/Day_1/Trial_002_0/binned_zscore.mat")
-  
-  # data
-  temp <- cbind(as.data.frame(t(readMat(filename_bh)[[1]])), as.data.frame(readMat(filename_zs)[[1]]))
-  
-  # name of the data(mouse name)
-  mouseN <- mice_name_O[i]
-  # Rename
-  name <- paste0("O_", substr(mouseN, nchar(mouseN) - 2, nchar(mouseN)))
-  dimnames(temp)[[2]][1:2] <- c("Y1", "Y2")
-  temp %<>% 
-    rename_at(vars(starts_with("V")), funs(str_replace(., "V", replacement = "X")))
-  
-  assign(name, temp)
-  
-  # remove temp from environment
-  rm("temp")
-}
-
-for (i in 3:6){
-  
-  filename_bh <- paste0("data/Opp_Sex/", mice_name_Z[i],  "/Day_2/Trial_002_0/binned_behavior.mat")
-  filename_zs <- paste0("data/Opp_Sex/", mice_name_Z[i],  "/Day_2/Trial_002_0/binned_zscore.mat")
-  
-  # data
-  temp <- cbind(as.data.frame(t(readMat(filename_bh)[[1]])), as.data.frame(readMat(filename_zs)[[1]]))
-  
-  # name of the data(mouse name)
-  mouseN <- mice_name_O[i]
-  # Rename
-  name <- paste0("O_", substr(mouseN, nchar(mouseN) - 2, nchar(mouseN)))
-  dimnames(temp)[[2]][1:2] <- c("Y1", "Y2")
-  temp %<>% 
-    rename_at(vars(starts_with("V")), funs(str_replace(., "V", replacement = "X")))
-  
   
   assign(name, temp)
   
